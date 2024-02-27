00202a5af885dbbf89e713eb4cd18504633a13a8
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-02-27 17:53:27 -0500

    Initial commit
    Initial commit. copied assignment 5, added new items to CustomList

---
+++ Assignment7/src/com/coderscampus/arraylist/CustomListApplication.java
@@ -0,0 +1,87 @@
+package com.coderscampus.arraylist;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+
+			CustomList<String> theList = new CustomArrayList<>();
+			
+			theList.add("element 1");
+			theList.add("element 2");
+			theList.add("element 3");
+			theList.add("element 4");
+			theList.add("element 5");
+			System.out.println(theList.getSize());
+			
+			theList.add("element 6");
+			theList.add("element 7");
+			theList.add("element 8");
+			theList.add("element 9");
+			theList.add("element 10");
+			System.out.println(theList.getSize());
+			
+			theList.add("element 11");
+			theList.add("element 12");
+			theList.add("element 13");
+			theList.add("element 14");
+			theList.add("element 15");
+			System.out.println(theList.getSize());
+			
+			theList.add("element 16");
+			theList.add("element 17");
+			theList.add("element 18");
+			theList.add("element 19");
+			theList.add("element 20");
+			System.out.println(theList.getSize());
+			
+			theList.add("element 21");
+			theList.add("element 22");
+			theList.add("element 23");
+			theList.add("element 24");
+			theList.add("element 25");
+			System.out.println(theList.getSize());
+			
+			theList.add("element 26");
+			theList.add("element 27");
+			theList.add("element 28");
+			theList.add("element 29");
+			theList.add("element 30");
+			theList.add("element 31");
+			theList.add("element 32");
+			theList.add("element 33");
+			theList.add("element 34");
+			theList.add("element 35");
+			theList.add("element 36");
+			theList.add("element 37");
+			theList.add("element 38");
+			theList.add("element 39");
+			theList.add("element 40");
+			theList.add("element 41");
+			theList.add("element 42");
+			theList.add("element 43");
+			theList.add("element 44");
+			theList.add("element 45");
+			theList.add("element 46");
+			theList.add("element 47");
+			theList.add("element 48");
+			theList.add("element 49");
+			theList.add("element 50");
+			theList.add("element 51");
+			theList.add("element 52");
+			theList.add("element 53");
+			theList.add("element 54");
+			theList.add("element 55");
+			theList.add("element 56");
+			theList.add("element 57");
+			theList.add("element 58");
+			theList.add("element 59");
+			theList.add("element 60");
+			System.out.println(theList.getSize());
+			
+			for(int i = 0; i < theList.getSize(); i++) {
+				System.out.println(theList.get(i));
+			}
+
+	}
+
+}
