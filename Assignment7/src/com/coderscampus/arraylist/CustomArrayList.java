0c848b16749afaec7c32edc39a969c99a662c68f
LordEricDraven <58068655+LordEricDraven@users.noreply.github.com>
2024-03-06 16:44:32 -0500

    updating Assignment 7
    added @Test to methods, changed IndexOutOfBoundsException to ArrayIndexOutOfBoundsException

--- Assignment7/src/com/coderscampus/arraylist/CustomArrayList.java
+++ Assignment7/src/com/coderscampus/arraylist/CustomArrayList.java
@@ -2,9 +2,12 @@ package com.coderscampus.arraylist;
 
 import java.util.Arrays;
 
+import org.junit.jupiter.api.Test;
+
 public class CustomArrayList<T> implements CustomList<T>{
 	Object[] items = new Object[10];
 
+	@Test
 	@Override
 	public boolean add(T item) {
 		for (int i = 0; i <= items.length; i++) {
@@ -19,6 +22,7 @@ public class CustomArrayList<T> implements CustomList<T>{
 		return false;
 	}
 
+	@Test
 	@Override
 	public int getSize() {
 		int size = 0;
@@ -32,13 +36,15 @@ public class CustomArrayList<T> implements CustomList<T>{
 	}
 
 	@SuppressWarnings("unchecked")
+	@Test
 	@Override
 	public T get(int index) {
 		return (T) items[index];
 	}
 
+	@Test
 	@Override
-	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+	public boolean add(int index, T item) throws ArrayIndexOutOfBoundsException {
 		Object[] itemList = new Object[items.length];
 		int i = 0;
 		
@@ -61,9 +67,10 @@ public class CustomArrayList<T> implements CustomList<T>{
 		return true;
 	}
 
+	@Test
 	@SuppressWarnings("unchecked")
 	@Override
-	public T remove(int index) throws IndexOutOfBoundsException {
+	public T remove(int index) throws ArrayIndexOutOfBoundsException {
 		Object removedItem = get(index);
 		Object[] obj = new Object[items.length];
 		int i = 0;
